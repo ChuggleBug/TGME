93ad052d3da5e930dcd187000c56f963ef117536
Jacob <jacobgutierrez752@gmail.com>
2025-03-08 13:21:45 -0800

    Change to hex codes

--- constants.py
+++ constants.py
@@ -1,20 +1,26 @@
 from enum import Enum, auto
 
 class Color(Enum):
+    DEFAULT = auto()
+    WHITE = auto()
+    GRAY = auto()
     RED = auto()
-    BLUE = auto()
     GREEN = auto()
+    BLUE = auto()
     YELLOW = auto()
     PURPLE = auto()
-    WHITE = auto()
-    DEFAULT = auto()
+    LIGHT_BLUE = auto()
+    ORANGE = auto()
 
-TK_COLOR_MAP = \
-    {Color.DEFAULT: 'white',
-     Color.WHITE: 'white',
-     Color.RED: 'red',
-     Color.GREEN: 'green',
-     Color.BLUE: 'blue',
-     Color.YELLOW: 'yellow',
-     Color.PURPLE: 'purple'
-     }
+TK_COLOR_MAP = {
+    Color.DEFAULT: '#FFFFFF',  # White
+    Color.WHITE: '#FFFFFF',    # White
+    Color.GRAY: '#808080',     # Gray
+    Color.RED: '#FF0000',      # Red
+    Color.GREEN: '#008000',    # Green
+    Color.BLUE: '#0000FF',     # Blue
+    Color.YELLOW: '#FFFF00',   # Yellow
+    Color.PURPLE: '#800080',   # Purple
+    Color.LIGHT_BLUE: '#ADD8E6', # Light Blue
+    Color.ORANGE: '#FFA500'    # Orange
+}
